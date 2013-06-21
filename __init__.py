0d230d0d94d29adeaa05b613ef23381b36c44b20
Morgan Fouesneau <morgan@fenrir>
2013-06-21 01:12:29 -0700

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+This package implements the No-U-Turn Sampler (NUTS) algorithm 6 from the NUTS
+paper (Hoffman & Gelman, 2011).
+
+Content
+-------
+
+The package mainly contains:
+  nuts6                     return samples using the NUTS
+  numerical_grad            return numerical estimate of the local gradient
+  emcee                     emcee NUTS sampler
+"""
+
+from .nuts import nuts6, numerical_grad
+from .emcee_nuts import NUTSSampler
